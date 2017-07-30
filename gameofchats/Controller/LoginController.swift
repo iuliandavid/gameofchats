a7d67db1139df6cb2ed7d93a49d8572d6e6184c0
Iulian David <david.iulian@gmail.com>
2017-07-30 20:43:57 +0300

    draft

---
+++ gameofchats/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  gameofchats
+//
+//  Created by iulian david on 7/30/17.
+//  Copyright © 2017 iulian david. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
