53c3f308703c5aaf5372ba1a85e8430f53008745
Iulian David <david.iulian@gmail.com>
2017-07-31 05:37:05 +0300

    login page start

--- gameofchats/Controller/ViewController.swift
+++ gameofchats/Controller/ViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
-
-   
+    
+    @objc func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
 
 }
